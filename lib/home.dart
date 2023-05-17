45a8b7aaadf6f229a81319d00a48f91e2d8e90ce
AndresAviles24 <andres.aviles09@gmail.com>
2023-05-17 10:34:37 -0400

    imagenes

--- lib/home.dart
+++ lib/home.dart
@@ -3,6 +3,8 @@ import 'dart:async';
 import 'dart:math';
 
 class WhackAMoleGame extends StatefulWidget {
+  const WhackAMoleGame({super.key});
+
   @override
   _WhackAMoleGameState createState() => _WhackAMoleGameState();
 }
@@ -31,7 +33,7 @@ class _WhackAMoleGameState extends State<WhackAMoleGame> {
     _totalMoles = 0;
     _totalHits = 0;
     _gameOver = false;
-    _timer = Timer.periodic(Duration(seconds: 1), (_) {
+    _timer = Timer.periodic(const Duration(seconds: 1), (_) {
       _generateMole();
     });
   }
@@ -48,7 +50,7 @@ class _WhackAMoleGameState extends State<WhackAMoleGame> {
     int index = random.nextInt(9);
     setState(() {
       _gameBoard[index] = true;
-      Future.delayed(Duration(seconds: 2), () {
+      Future.delayed(const Duration(seconds: 5), () {
         if (!_gameOver) {
           setState(() {
             _gameBoard[index] = false;
@@ -58,6 +60,7 @@ class _WhackAMoleGameState extends State<WhackAMoleGame> {
     });
   }
 
+  //TODO agregar el efecto de sonido cuando se golpea al quijote
   void _hitMole(int index) {
     setState(() {
       _gameBoard[index] = false;
@@ -65,6 +68,25 @@ class _WhackAMoleGameState extends State<WhackAMoleGame> {
 
       if (_totalHits >= 10) {
         _endGame();
+        _gameBoard = List.filled(9, false);
+        showDialog(
+            context: context,
+            builder: (BuildContext context) {
+              return AlertDialog(
+                title: const Text("Has ganado!"),
+                content: const Text(
+                    "La introducción es la parte inicial de un texto, sin importar la temática o el tipo de texto que estemos escribiendo."),
+                actions: <Widget>[
+                  TextButton(
+                    child: const Text("Cerrar"),
+                    onPressed: () {
+                      Navigator.of(context).pop();
+                    },
+                  ),
+                ],
+              );
+            });
+        //_totalHits = 0;
       }
     });
   }
@@ -72,8 +94,10 @@ class _WhackAMoleGameState extends State<WhackAMoleGame> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.green.shade300,
       appBar: AppBar(
-        title: Text("Whack-a-Mole"),
+        title: const Text("Whack-a-Mole"),
+        backgroundColor: Colors.brown,
       ),
       body: Center(
         child: Column(
@@ -81,14 +105,15 @@ class _WhackAMoleGameState extends State<WhackAMoleGame> {
           children: <Widget>[
             GridView.builder(
               shrinkWrap: true,
-              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
                 crossAxisCount: 3,
               ),
               itemBuilder: (context, index) {
+                /*
                 return ElevatedButton(
                   style: ElevatedButton.styleFrom(
-                    primary: _gameBoard[index] ? Colors.red : Colors.black,
-                    minimumSize: Size(80, 80),
+                    backgroundColor: _gameBoard[index] ? Colors.red : Colors.black,
+                    minimumSize: const Size(80, 80),
                   ),
                   /*
                   onPressed: _gameOver
@@ -101,33 +126,86 @@ class _WhackAMoleGameState extends State<WhackAMoleGame> {
                   onPressed: () {
                     if (_gameBoard[index]) {
                       setState(() {
-                        _hitMole(index);
+                          _hitMole(index);
                       });
                     }
                   },
                   child: Container(),
                 );
+                */
+                //reemplazo
+                /*
+                return ElevatedButton(
+                  onPressed: () {
+                    if (_gameBoard[index]) {
+                      setState(() {
+                        _hitMole(index);
+                      });
+                    }
+                  },
+                  style: ElevatedButton.styleFrom(
+                    foregroundColor: Colors.green.shade300, backgroundColor: Colors.green.shade300,
+                    minimumSize: const Size(80, 80),
+                  ),
+                  child: _gameBoard[index]
+                      ? Image.network('https://www.bbva.com/wp-content/uploads/2016/03/don-quijote-de-la-mancha-1920x0-c-f-1.jpg')//boton a golpear
+                      : Image.network('https://insulabaranaria.files.wordpress.com/2012/08/imagenquijote1.jpg'),//no golpeable
+                );
+*/
+                return ElevatedButton(
+                  onPressed: () {
+                    if (_gameBoard[index]) {
+                      setState(() {
+                        _hitMole(index);
+                      });
+                    }
+                  },
+                  style: ElevatedButton.styleFrom(
+                    foregroundColor: Colors.green.shade300,
+                    backgroundColor: Colors.green.shade300,
+                    minimumSize: const Size(80, 80),
+                  ),
+                  child: AnimatedSwitcher(
+                    duration: const Duration(milliseconds: 500),
+                    transitionBuilder:
+                        (Widget child, Animation<double> animation) {
+                      return ScaleTransition(
+                        scale: animation,
+                        child: child,
+                      );
+                    },
+                    child: _gameBoard[index]
+                        ? Image.network(
+                            'https://www.bbva.com/wp-content/uploads/2016/03/don-quijote-de-la-mancha-1920x0-c-f-1.jpg',
+                            key: UniqueKey(),
+                          )
+                        : Image.network(
+                            'https://insulabaranaria.files.wordpress.com/2012/08/imagenquijote1.jpg',
+                            //key: UniqueKey(),
+                          ),
+                  ),
+                );
               },
               itemCount: 9,
             ),
-            SizedBox(
+            const SizedBox(
               height: 20,
             ),
             Text(
-              "Golpes totales: $_totalHits",
-              style: TextStyle(
+              "Puntuacion: $_totalHits",
+              style: const TextStyle(
                 fontSize: 20,
                 fontWeight: FontWeight.bold,
               ),
             ),
-            SizedBox(
+            const SizedBox(
               height: 20,
             ),
             ElevatedButton(
               onPressed: _gameOver ? _startGame : null,
               child: Text(
-                _gameOver ? "Play Again" : "Start Game",
-                style: TextStyle(
+                _gameOver ? "Volver a jugar" : "Volver a jugar",
+                style: const TextStyle(
                   fontSize: 20,
                   fontWeight: FontWeight.bold,
                 ),
