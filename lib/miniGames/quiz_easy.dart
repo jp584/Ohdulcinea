9908989ae01fb18aeb23c917350d50524cf527e2
AndresAviles24 <andres.aviles09@gmail.com>
2023-05-19 09:27:15 -0400

    const agregados
    Para mejorar performance

--- lib/miniGames/quiz_easy.dart
+++ lib/miniGames/quiz_easy.dart
@@ -26,11 +26,11 @@ class _HomeState extends State<Home> {
       // adding to the score tracker on top
       _scoreTracker.add(
         answerScore
-            ? Icon(
+            ? const Icon(
                 Icons.check_circle,
                 color: Colors.green,
               )
-            : Icon(
+            : const Icon(
                 Icons.clear,
                 color: Colors.red,
               ),
@@ -67,7 +67,7 @@ class _HomeState extends State<Home> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(
+        title: const Text(
           'Quiz Minigame',
           style: TextStyle(color: Colors.white),
         ),
@@ -88,8 +88,10 @@ class _HomeState extends State<Home> {
             Container(
               width: double.infinity,
               height: 130.0,
-              margin: EdgeInsets.only(bottom: 10.0, left: 30.0, right: 30.0),
-              padding: EdgeInsets.symmetric(horizontal: 50.0, vertical: 20.0),
+              margin:
+                  const EdgeInsets.only(bottom: 10.0, left: 30.0, right: 30.0),
+              padding:
+                  const EdgeInsets.symmetric(horizontal: 50.0, vertical: 20.0),
               decoration: BoxDecoration(
                 color: Colors.deepOrange,
                 borderRadius: BorderRadius.circular(10.0),
@@ -98,7 +100,7 @@ class _HomeState extends State<Home> {
                 child: Text(
                   _questions[_questionIndex]['question'].toString(),
                   textAlign: TextAlign.center,
-                  style: TextStyle(
+                  style: const TextStyle(
                     fontSize: 20.0,
                     color: Colors.white,
                     fontWeight: FontWeight.bold,
@@ -122,15 +124,15 @@ class _HomeState extends State<Home> {
                 },
               ),
             ),
-            SizedBox(height: 20.0),
+            const SizedBox(height: 20.0),
             ElevatedButton(
               style: ElevatedButton.styleFrom(
-                minimumSize: Size(double.infinity, 40.0),
+                minimumSize: const Size(double.infinity, 40.0),
               ),
               onPressed: () {
                 if (!answerWasSelected) {
                   ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-                    content: Text(
+                    content: const Text(
                         'Please select an answer before moving to the next question.'),
                   ));
                   return;
@@ -140,10 +142,11 @@ class _HomeState extends State<Home> {
               child: Text(endOfQuiz ? 'Restart Quiz' : 'Next Question'),
             ),
             Container(
-              padding: EdgeInsets.all(20.0),
+              padding: const EdgeInsets.all(20.0),
               child: Text(
                 '${_totalScore.toString()}/${_questions.length}',
-                style: TextStyle(fontSize: 40.0, fontWeight: FontWeight.bold),
+                style: const TextStyle(
+                    fontSize: 40.0, fontWeight: FontWeight.bold),
               ),
             ),
             if (answerWasSelected && !endOfQuiz)
@@ -154,7 +157,7 @@ class _HomeState extends State<Home> {
                 child: Center(
                   child: Text(
                     correctAnswerSelected ? 'Correcto!' : 'Incorrecto...',
-                    style: TextStyle(
+                    style: const TextStyle(
                       fontSize: 20.0,
                       fontWeight: FontWeight.bold,
                       color: Colors.white,
@@ -279,8 +282,8 @@ class Answer extends StatelessWidget {
     return InkWell(
       onTap: answerTap,
       child: Container(
-        padding: EdgeInsets.all(15.0),
-        margin: EdgeInsets.symmetric(vertical: 5.0, horizontal: 30.0),
+        padding: const EdgeInsets.all(15.0),
+        margin: const EdgeInsets.symmetric(vertical: 5.0, horizontal: 30.0),
         width: double.infinity,
         decoration: BoxDecoration(
           color: answerColor,
@@ -289,7 +292,7 @@ class Answer extends StatelessWidget {
         ),
         child: Text(
           answerText,
-          style: TextStyle(
+          style: const TextStyle(
             fontSize: 15.0,
           ),
         ),
